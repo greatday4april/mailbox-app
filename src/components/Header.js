3fa3625c410379653bdf6ff61423a98ad476b409
Jan Chris Tacbianan <jtacbianan@initiateid.com>
2021-04-23 11:20:18 -0400

    Refactored Google Auth Implementation.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
-
+import AccountContext, { AccountProvider } from '../utils/AccountContext'
 import { styles as GlobalStyles } from "../utils/styles";
 import React from "react";
 
 export const Header = (props) => {
+  const {account, setAccount} = React.useContext(AccountContext);
+
   return (
     <View style={styles.header}>
       <Text style={styles.headerText}>{props.title}</Text>
+      <Text style={styles.userText}>Signed in as: {account.email}</Text>
     </View>
   );
 };
@@ -15,13 +18,16 @@ const styles = StyleSheet.create({
   header: {
     height: 30,
     marginTop: 40,
-    marginLeft: 150,
+    textAlignVertical: "center",
     textAlign: "center",
     backgroundColor: "transparent",
   },
   headerText: {
-    fontFamily:'Gill Sans',
+    fontFamily:'Roboto',
     fontSize: 30,
     color: 'powderblue',
   },
+  userText: {
+    color: "white"
+  }
 });
